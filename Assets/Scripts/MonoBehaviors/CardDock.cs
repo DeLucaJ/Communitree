573d5fc21eb090f5f696ea1eaeec8aaf1ae7866c
DeLucaJ <delucaj96@gmail.com>
2019-01-26 14:22:24 -0800

    Card Dock Prefab, CreateCards Method

---
+++ Assets/Scripts/MonoBehaviors/CardDock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDock : MonoBehaviour
+{
+    public Vector2Int[] cardPoints;
+    public GameObject cardPrefab;
+
+    private GameObject[] cards = new GameObject[3];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateCards();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateCards()
+    {
+        for (int i = 0; i < cardPoints.Length; i++){
+            cards[i] = Instantiate(cardPrefab, 
+                transform.position + new Vector3(cardPoints[i].x, cardPoints[i].y, 0), 
+                Quaternion.identity);
+            cards[i].transform.SetParent(gameObject.transform);
+        }
+    }
+}
