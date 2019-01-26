246c6fd041cbbf30e011cb3506f92dcc7b86485f
DeLucaJ <delucaj96@gmail.com>
2019-01-26 01:42:13 -0800

    Base Card image & Card Scriptables

---
+++ Assets/Scripts/MonoBehaviors/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public Action action1;
+    public Action aciton2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Action.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card Action", menuName = "Card Action")]
+public class Action : ScriptableObject
+{
+    public enum ActionType { branch, trunk, fruit, leaf };
+
+    public int lowerBound;
+    public int upperBound;
+    public Sprite image;
+    public ActionType type;
+    public Color color;
+}
