1ace47df00bd7d65a754728d142ed1e17b6042a2
DeLucaJ <delucaj96@gmail.com>
2019-01-27 11:05:31 -0800

    Fixed Typo

--- Assets/Scripts/ScriptableObjects/Inhabitants.cs
+++ Assets/Scripts/ScriptableObjects/Inhabitants.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New CInhabitant", menuName = "Inhabitant")]
+[CreateAssetMenu(fileName = "New Inhabitant", menuName = "Inhabitant")]
 public class Inhabitants : ScriptableObject
 {
     public Sprite sprite;
