6b3252dcdc905af51c28fb79613163c9728b93a0
DeLucaJ <18635544+DeLucaJ@users.noreply.github.com>
2019-11-02 13:36:42 -0700

    Adds Basic Card Prefab

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public CardType type;
+    public int value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayCard() {
+        type.Trigger(new object[] { value });
+    }
+}
---
+++ Assets/Scripts/Cards/CardType.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCardType", menuName = "CardType", order = 1)]
+public class CardType : ScriptableObject
+{
+    public string eventName;
+    public Sprite art;
+
+    public void Trigger(object[] values = null) {
+        EventHandler.TriggerEvent(eventName, values);
+    }
+}
