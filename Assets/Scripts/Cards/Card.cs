1a998bf9266b0b1d63a51a6a6cc319fafe04fa46
DeLucaJ <18635544+DeLucaJ@users.noreply.github.com>
2019-11-09 11:26:13 -0800

    Adds the mouse event functions to Card.cs

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -6,19 +6,37 @@ public class Card : MonoBehaviour
 {
     public CardType cardType;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void PlayCard() {
         cardType.Trigger(new object[] {});
+    }
+
+    // Called when the mouse clicks on the Card
+    void OnMouseDown() {
+
+    }
+
+    // Called when the mouse releases a click on the Card
+    void OnMouseUp() {
+
+    }
+
+    // Called every frame the mouse is over the Card
+    void OnMouseOver() {
+
+    }
+
+    // Called when the mouse first goes over the Card image
+    void OnMouseEnter() {
+
+    }
+
+    // Called when the mouse leaves the Card Image
+    void OnMouseExit() {
+
+    }
+
+    // Called every frame while the mouse is holding a click on the Card
+    void OnMouseDrag() {
+
     }
 }
