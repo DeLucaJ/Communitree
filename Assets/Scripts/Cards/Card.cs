fa9fa550fb73340228d701c38bbf386b59a4c4e1
DeLucaJ <18635544+DeLucaJ@users.noreply.github.com>
2019-11-09 12:24:40 -0800

    Adds CardDock and Hand Files

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -17,26 +17,26 @@ public class Card : MonoBehaviour
 
     // Called when the mouse releases a click on the Card
     void OnMouseUp() {
-
+        // if not in dock, play card
     }
 
     // Called every frame the mouse is over the Card
     void OnMouseOver() {
-
+        // Make the Frame glow
     }
 
     // Called when the mouse first goes over the Card image
     void OnMouseEnter() {
-
+        // Zoom on Card
     }
 
     // Called when the mouse leaves the Card Image
     void OnMouseExit() {
-
+        // Return to Normal Size/Position/Shaders
     }
 
     // Called every frame while the mouse is holding a click on the Card
     void OnMouseDrag() {
-
+        // Card follows the mouse
     }
 }
